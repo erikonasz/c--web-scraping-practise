ea5b4191a535a475a9a081749b48bd248fbdfcc5
erikonasz <arcikauskas.erikas@gmail.com>
2024-01-22 16:16:58 +0200

    first commit

---
+++ Start/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Send get request to weather.com
+            string url = "https://weather.com/lt-LT/orai/siandien/l/LHXX0005:1:LH?Goto=Redirected";
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url).Result;
+
+            // Use HtmlDocument type
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            // Fix typo in SelectSingleNode
+            var temperatureElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='CurrentConditions--tempValue--MHmYY']");
+            var temperature = temperatureElement.InnerText.Trim();
+            Console.WriteLine("Temperature: " + temperature);
+
+            // Get conditions
+        }
+    }
+}
